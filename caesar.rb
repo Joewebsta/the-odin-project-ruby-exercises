59588d9f3ffd58cdeffe327c24c5bbb069600fdb
Joe Webster <joseph.scott.webster@gmail.com>
2020-06-24 18:19:29 -0400

    Complete caesar cipher exercise

--- caesar.rb
+++ caesar.rb
@@ -1,12 +1,19 @@
-require 'pry'
-
 def isNonLetter?(byte)
   return byte < 65 || byte > 90 && byte < 97 || byte > 122
 end
 
 def remove_shift_cycles(shift)
-  shift = shift - 26
-  shift > 26 ? leftover(shift) : shift
+  shift -= 26
+  shift > 26 ? remove_shift_cycles(shift) : shift
+end
+
+def exceeds_z?(byte, shifted_byte)
+  if (byte >= 65 && byte <= 90 && shifted_byte > 90) || 
+     (byte >= 97 && byte <= 122 && shifted_byte > 122)
+    shifted_byte -= 26
+  else
+    shifted_byte
+  end 
 end
 
 def caesar_cipher(string, shift)
@@ -14,68 +21,15 @@ def caesar_cipher(string, shift)
   shift > 25 ? shift = remove_shift_cycles(shift) : shift
 
   shifted_bytes = bytes.map do |byte|
-    isNonLetter?(byte) ? byte : byte + shift
+    if (isNonLetter?(byte))
+      byte.chr
+    else
+      shifted_byte = byte + shift
+      sanitized_shifted_byte = exceeds_z?(byte, shifted_byte).chr
+    end
   end
 
-  p shifted_bytes
+  p shifted_bytes.join()
 end
 
-caesar_cipher('a!b#c]d}', 0)
-caesar_cipher('a!b#c]d}', 50)
-
-
-  # < 65 SPECIAL
-  # #65-90  A-Z
-  # >90 && <97 SPECIAL
-  # #97-122 a-z
-  # > 122 SPECIAL
-
-
-  # if shift > 26
-  #   shift = shift - shift / 26
-  # end
-
-  # ordArr = string.bytes
-
-  # shiftedArr = ordArr.map do |num|
-  #   # if num >= 32 && num <= 47 
-  #   #   num.chr
-  #   # end
-    
-  #   shifted_num = num + shift
-
-
-
-
-
-  #   if (num >= 97 && num <= 122 && shifted_num > 122) ||
-  #      (num >= 65 && num <= 90 && shifted_num > 90)
-  #     shifted_num = shifted_num - 26
-  #   end
-
-
-      
-  #   #   shifted_num -= 26
-
-  #   # # (shifted_num >= 97 && shifted_num <= 122) || 
-  #   # #    (shifted_num >= 65 && shifted_num <= 90)
-
-  #   # shifted_num.chr
-  #   shifted_num.chr
-  # end
-
-  # shiftedArr.join('')
-
-# 32 - 47 special characters
-
-
-# split string into individual letters
-# convert individual letters into ascii code integer values
-
-# increase ascii integer values by the shift amount
-  # Determine upper and lower bounds of ascii integer values
-  # If shifted amount increases beyond upper bound decrease by 26 to get correct letter
-  # ignore spaces and punctuation
-# 
-
-
+caesar_cipher("What a string!", 5)

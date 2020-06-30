f1212655a9f4457639012e9a084da0d7a36f0082
Joe Webster <joseph.scott.webster@gmail.com>
2020-06-30 18:07:42 -0400

    Update variable names and extract p methods from sort bubble function

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,29 +1,27 @@
 def bubble_sort(arr)
   sortCount = 0
 
-  sortedArr = arr.each_with_index do |item, idx| 
-    next if idx == arr.length - 1
+  sortedArr = arr.each_with_index do |num, i| 
+    next if i == arr.length - 1
 
-    item1 = item
-    item2 = arr[idx + 1]
+    num1 = num
+    num2 = arr[i+1]
 
-    if item2 < item1
-      arr[idx] = item2
-      arr[idx + 1] = item1
+    if num2 < num1
+      arr[i] = num2
+      arr[i+1] = num1
       sortCount += 1
     end
     arr
   end
 
-  p sortedArr
-
   if sortCount == 0
-    p sortedArr
+    sortedArr
   else
     sortCount = 0
     bubble_sort(sortedArr)
   end
 end
 
-bubble_sort([0,2,2,3,4,78])
-bubble_sort([4,3,78,2,0,2,39,11,21,13,77,97,51])
+p bubble_sort([0,2,2,3,4,78])
+p bubble_sort([4,3,78,2,0,2,39,11,21,13,77,97,51])
